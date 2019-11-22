2f337f61305393f11ce360afb8e419f419ad1817
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 09:58:13 -0800

    added: completed field to the todo

--- server/todos/models.py
+++ server/todos/models.py
@@ -1,8 +1,8 @@
-from django.db.models import Model, CharField, DateTimeField
+from django.db.models import Model, CharField, BooleanField
 
 # Create your models here.
 
 class Todo(Model):
-    created_on = DateTimeField(auto_now_add=True)
     title = CharField(max_length=30)
     body = CharField(max_length=300)
+    completed = BooleanField(default=False)
