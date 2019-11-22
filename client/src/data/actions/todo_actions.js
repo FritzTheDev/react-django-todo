590b18311d1a5f7db4ce8852811e0f1cc4f15e40
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 15:39:45 -0800

    hardcoded URL for now

--- client/src/data/actions/todo_actions.js
+++ client/src/data/actions/todo_actions.js
@@ -6,9 +6,10 @@ export const ADD_TODO_SUCCESS = 'ADD_TODO_SUCCESS';
 export const ADD_TODO_FAILURE = 'ADD_TODO_FAILURE';
 
 export const requestTodos = () => (dispatch) => {
-    fetch(`${process.env.SERVER_ROOT_URL}/todos`)
-        .then(data => dispatch({ type: REQUEST_TODOS_SUCCESS, payload: { todos: data } }))
-        .catch(error => dispatch({ type: REQUEST_TODOS_FAILURE, payload: { error: error.toString() }}));
+    fetch(`http://localhost:8000/todos`)
+        .then(res => res.json()
+            .then(data => dispatch({ type: REQUEST_TODOS_SUCCESS, payload: { todos: data } }))
+            .catch(error => dispatch({ type: REQUEST_TODOS_FAILURE, payload: { error: error() }})));
 }
 
 export const addTodo = (title, body) => (dispatch) => {
