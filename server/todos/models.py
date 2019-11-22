a7269ce9833dd8f668132901cf1062a5b61c17aa
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 09:35:34 -0800

    added: basic todo model

--- server/todos/models.py
+++ server/todos/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.db.models import Model, CharField, DateTimeField
 
 # Create your models here.
+
+class Todo(Model):
+    created_on = DateTimeField(auto_now_add=True)
+    title = CharField(max_length=30)
+    body = CharField(max_length=300)
