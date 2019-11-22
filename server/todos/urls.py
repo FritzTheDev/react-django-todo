aaf6fe2a9bcf94c33a8e812c14d2379012194467
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 09:49:01 -0800

    changed: swapped to using a router in todo urls

--- server/todos/urls.py
+++ server/todos/urls.py
@@ -1,19 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views.todo_view_set import TodoViewSet
 
-todo_list = TodoViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
-
-todo_detail = TodoViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+router = DefaultRouter()
+router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
-    path('', todo_list, name="todo-list"),
-    path('<int:pk>/', todo_detail, name="todo-detail"),
+    path('', include(router.urls)),
 ]
