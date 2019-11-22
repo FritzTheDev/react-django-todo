101f3997f83f29da65fba20c1c0baf27f4abfe27
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 10:55:18 -0800

    started work on the todoListPage

---
+++ client/src/pages/TodoList.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTodoList } from '../data/reducers/todo_slice';
+import { rootState } from ''
