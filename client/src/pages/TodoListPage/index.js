86f89c8fff585315cc1a438e29406d7c67394f7b
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 14:20:33 -0800

    fix: added dispatch to the start of the propnames to fix naming bug

--- client/src/pages/TodoListPage/index.js
+++ client/src/pages/TodoListPage/index.js
@@ -4,22 +4,40 @@ import { connect } from "react-redux";
 import { addTodo, markTodoCompleted, markTodoNotCompleted, deleteTodo } from '../../data/actions/todo_actions';
 
 const BaseTodoListPage = (props) => {
-    console.log(props);
     console.log(props.todos);
-    return (<>
-        <button onClick={() => addTodo('test title', 'best body')}></button>
-        <h1>{props.todos[0] && props.todos[0].title}</h1>
-    </>);
+    return (
+        <>
+            <button onClick={() => props.dispatchAddTodo('test title', 'best body')}>Add Todo</button>
+            <button onClick={() => props.dispatchDeleteTodo(0)}>Remove First Todo</button>
+        </>
+    );
 }
 
-export const TodoListPage = connect(
-    (state) => ({
-        todos: state.todos
-    }),
-    (dispatch) => ({
-        addTodo: (title, body) => dispatch(addTodo(title, body)),
-        markTodoCompleted: (index) => dispatch(markTodoCompleted(index)),
-        markTodoNotCompleted: (index) => dispatch(markTodoNotCompleted(index)),
-        deleteTodo: (index) => dispatch(deleteTodo(index))
-    })
-)(BaseTodoListPage);
+const mapStateToProps = state => {
+    return {
+        todos: state.todos,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        dispatchAddTodo: (title, body) => dispatch(addTodo(title, body)),
+        dispatchMarkTodoCompleted: (index) => dispatch(markTodoCompleted(index)),
+        dispatchMarkTodoNotCompleted: (index) => dispatch(markTodoNotCompleted(index)),
+        dispatchDeleteTodo: (index) => dispatch(deleteTodo(index)),
+    }
+}
+
+export const TodoListPage = connect(mapStateToProps, mapDispatchToProps)(BaseTodoListPage);
+
+// export const TodoListPage = connect(
+//     (state) => ({
+//         todos: state.todos
+//     }),
+//     (dispatch) => ({
+//         addTodo: (title, body) => dispatch(addTodo(title, body)),
+//         markTodoCompleted: (index) => dispatch(markTodoCompleted(index)),
+//         markTodoNotCompleted: (index) => dispatch(markTodoNotCompleted(index)),
+//         deleteTodo: (index) => dispatch(deleteTodo(index))
+//     })
+// )(BaseTodoListPage);
