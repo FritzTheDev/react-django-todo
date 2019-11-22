0139935a28b34a79025a8ff8f8878b38454ec766
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 15:42:07 -0800

    setting up to make an actual UI

--- client/src/pages/TodoListPage/index.js
+++ client/src/pages/TodoListPage/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
 import { connect } from "react-redux";
+import { Container } from "react-bootstrap";
 
-import { addTodo, requestTodos } from '../../data/actions/todo_actions';
+import { addTodo, requestTodos, toggleTodo } from '../../data/actions/todo_actions';
+
+const renderTodoList = (toggleTodo, )
 
 const BaseTodoListPage = ({ todos, dispatchAddTodo, dispatchRequestTodos }) => {
     useEffect(() => {
@@ -25,6 +28,7 @@ const mapDispatchToProps = dispatch => {
     return {
         dispatchAddTodo: (title, body) => dispatch(addTodo(title, body)),
         dispatchRequestTodos: () => dispatch(requestTodos()),
+        dispatchToggleTodo: (id) => dispatch(toggleTodo(id))
     }
 }
 
