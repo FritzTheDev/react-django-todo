8b7f6558cf155330caad648cdd3a06164f44dc49
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 15:10:22 -0800

    progress: building out the reducer

---
+++ client/src/data/reducers/todos.js
@@ -0,0 +1,49 @@
+import { ADD_TODO_SUCCESS, ADD_TODO_FAILURE, REQUEST_TODOS_ATTEMPT, REQUEST_TODOS_SUCCESS, REQUEST_TODOS_FAILURE, TOGGLE_TODO_SUCCESS, TOGGLE_TODO_FAILURE } from "../actions/todo_actions";
+
+const initialState = { todos: [], error: null };
+
+export function todos = (state = initialState, action) => {
+  switch(action.type) {
+    case REQUEST_TODOS_SUCCESS:
+      return { todos: action.payload.todos, error: null }
+    case REQUEST_TODOS_FAILURE:
+      return { todos: [], error: action.payload.error }
+
+  }
+}
+
+// import {
+//   ADD_TODO,
+//   DELETE_TODO,
+//   MARK_TODO_COMPLETED,
+//   MARK_TODO_NOT_COMPLETED
+// } from "../actions/todo_actions";
+
+// const initialState = [];
+
+// export function todos(state = initialState, action) {
+//   switch (action.type) {
+//     // Adds TODO to end of array
+//     case ADD_TODO: 
+//       return [...state, { title: action.payload.title, body: action.payload.body, completed: false }];
+//     // Deletes TODO from specified index.
+//     case DELETE_TODO: 
+//       return state.filter((val, index) => index !== action.payload.index);
+//     // Marks TOOD as completed.
+//     case MARK_TODO_COMPLETED:
+//       return [
+//         ...state.slice(0, action.payload.index),
+//         { ...state[action.payload.index], completed: true },
+//         ...state.slice(action.payload.index + 1),
+//       ];
+//     // Marks TODO as not completed.
+//     case MARK_TODO_NOT_COMPLETED:
+//         return [
+//           ...state.slice(0, action.payload.index),
+//           { ...state[action.payload.index], completed: false },
+//           ...state.slice(action.payload.index + 1),
+//         ];
+//     default:
+//       return state;
+//   }
+// }
