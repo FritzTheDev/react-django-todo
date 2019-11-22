5051de40c6cc81eb0e14df96e0b021f601651b4d
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 09:36:55 -0800

    addded: queryset & serializer_class to TodoViewSet

--- server/todos/views/todo_view_set.py
+++ server/todos/views/todo_view_set.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from todos.models import Todo
+from todos.serializers import TodoSerializer
 
 class TodoViewSet(ModelViewSet):
-    # queryset = 
-    # serializer_class = 
+    queryset = Todo
+    serializer_class = TodoSerializer
