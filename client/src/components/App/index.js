435139907ceacb7650676c72da10988a3ee7d14f
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 13:46:53 -0800

    fix: data/store import fix

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 
-import { store } from "../data/store";
+import { store } from "../../data/store";
 import { Router } from "../Router";
 
 export const App = () => {
