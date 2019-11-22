c9193fec70f0178421244becdfa9feca3fb14949
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 11:37:30 -0800

    change: todo_actions swapped to using payloads for convetion.

--- client/src/data/actions/todo_actions.js
+++ client/src/data/actions/todo_actions.js
@@ -5,17 +5,17 @@ export const MARK_TODO_COMPLETED = 'MARK_TODO_COMPLETED';
 export const DELETE_TODO = 'DELETE_TODO';
 
 export function addTodo(title, body) {
-    return { type: ADD_TODO, title, body }
+    return { type: ADD_TODO, payload: { title, body } }
 }
 
 export function markTodoNotCompleted(index) {
-    return { type: MARK_TODO_NOT_COMPLETED, index }
+    return { type: MARK_TODO_NOT_COMPLETED, payload: { index } }
 }
 
 export function markTodoCompleted(index) {
-    return { type: MARK_TODO_COMPLETED, index }
+    return { type: MARK_TODO_COMPLETED, payload: { index } }
 }
 
 export function deleteTodo(index) {
-    return { type: DELETE_TODO, index }
+    return { type: DELETE_TODO, payload: { index } }
 }
