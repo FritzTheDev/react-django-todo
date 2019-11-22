8d70c8cdc09f2c8c9ab632e45bcc45db50b8d402
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 13:29:42 -0800

    fix: added rootReducer properly in store.js

--- client/src/data/store.js
+++ client/src/data/store.js
@@ -1,6 +1,4 @@
 import { createStore } from 'redux';
 import { rootReducer } from './reducers';
 
-export const store = createStore({
-    reducer: rootReducer
-});
+export const store = createStore(rootReducer);
