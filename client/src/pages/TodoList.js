457e247995f3896d086ebbf0a4e1caaa3da065a4
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 13:44:11 -0800

    progress: building out TodoList Page

--- client/src/pages/TodoList.js
+++ client/src/pages/TodoList.js
@@ -1,4 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getTodoList } from '../data/reducers/todo_slice';
-import { rootState } from ''
+import React from 'react';
+import { connect } from "react-redux";
+
+import { addTodo, markTodoCompleted, markTodoNotCompleted, deleteTodo } from '../data/actions/todo_actions';
+
+const TodoListPage = (props) => {
+    console.log(props);
+    return (<div />);
+}
+
+connect(
+    (state) => ({
+        todos: state.todos
+    }),
+    (dispatch) => ({
+        addTodo: (title, body) => addTodo(title, body),
+        markTodoCompleted: (index) => markTodoCompleted(index),
+        markTodoNotCompleted: (index) => markTodoNotCompleted(index),
+        deleteTodo: (index) => deleteTodo(index)
+    })
+)(TodoListPage);
