99c98c66f48dda222401ca52957ff05695a6ca3b
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 09:46:42 -0800

    added: url configuration for todos

--- server/todos/urls.py
+++ server/todos/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
+from .views.todo_view_set import TodoViewSet
+
+todo_list = TodoViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+todo_detail = TodoViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
 
 urlpatterns = [
-    
+    path('', todo_list, name="todo-list"),
+    path('<int:pk>/', todo_detail, name="todo-detail"),
 ]
