c5ee8563fba879c7b8a5af4a578606e8244165a2
Frederick Johnson <fritzj96@gmail.com>
2019-11-22 17:23:36 -0800

    hardcoded url for toggleTodo action

--- client/src/data/actions/todo_actions.js
+++ client/src/data/actions/todo_actions.js
@@ -6,14 +6,14 @@ export const ADD_TODO_SUCCESS = 'ADD_TODO_SUCCESS';
 export const ADD_TODO_FAILURE = 'ADD_TODO_FAILURE';
 
 export const requestTodos = () => (dispatch) => {
-    fetch(`http://localhost:8000/todos`)
+    fetch(`http://localhost:8000/todos/`)
         .then(res => res.json()
             .then(data => dispatch({ type: REQUEST_TODOS_SUCCESS, payload: { todos: data } }))
             .catch(error => dispatch({ type: REQUEST_TODOS_FAILURE, payload: { error: error() }})));
 }
 
 export const addTodo = (title, body) => (dispatch) => {
-    fetch(`${process.env.SERVER_ROOT_URL}/todos`, { method: 'POST', body: { title, body } })
+    fetch(`${process.env.SERVER_ROOT_URL}/todos/`, { method: 'POST', body: { title, body } })
         .then(data => dispatch({ type: ADD_TODO_SUCCESS, payload: { todo: data } }))
         .catch(error => dispatch({ type: ADD_TODO_FAILURE, payload: { error: error.toString() } }));
 }
@@ -21,7 +21,8 @@ export const addTodo = (title, body) => (dispatch) => {
 export const toggleTodo = (id) => (dispatch, getState) => {
     const list = getState()['todos']['list'];
     const todoIsCompleted = list.filter(val => val.id === id);
-    fetch(`${process.env.SERVER_ROOT_URL}/todos/${id}`, { method: 'PATCH'}, { completed: !todoIsCompleted })
+    console.log(todoIsCompleted);
+    fetch(`http://localhost:8000/todos/${id}/`, { method: 'PATCH'}, { completed: !todoIsCompleted.id })
         .then(data => dispatch({ type: TOGGLE_TODO_SUCCESS, payload: { todo: data } }))
         .catch(error => dispatch({ type: TOGGLE_TODO_FAILURE, payload: { error: error.toString() } }));
 }
